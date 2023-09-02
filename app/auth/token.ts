a7982820a9fd32d20c792e1bc652f7f4a605b819
Gavin <gsman707@gmail.com>
2023-09-02 01:20:58 -0700

    start

---
+++ app/auth/token.ts
@@ -0,0 +1,11 @@
+import tokenResponse from "../models/auth/tokenResponse";
+import userAuthInfo from "../models/auth/userAuthInfo";
+
+const Token = {
+    createJWT(userInfo:userAuthInfo) {
+        
+    }
+}
+
+
+export default Token
---
+++ dist/auth/token.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const Token = {
+    createJWT(userInfo) {
+    }
+};
+exports.default = Token;
